ac3310a231cdc9768d3cafd144abed6e72ec1509
yair0666 <166673872+yair0666@users.noreply.github.com>
2024-05-30 01:27:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const randomMessage = moodMessages[mood][randomIndex];
             return `${randomMessage} Recuerda que te quiero muchito, mi Dairita ❤️`;
         } else {
-           
+            return `Siempre te apoyaré y estaré aquí para ti. Recuerda que te quiero muchito, mi Dairita ❤️`;
         }
     }
 });
